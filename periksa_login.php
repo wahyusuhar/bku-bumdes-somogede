5180b1b957e2e1be889c7eb4ee7c1006a80c6710
wahyusuhar <wahyuwakteng@gmail.com>
2025-08-26 23:03:50 +0700

    fix : add minor change to periksa_login.php

--- periksa_login.php
+++ periksa_login.php
@@ -24,8 +24,8 @@ if($cek > 0){
 		$_SESSION['status'] = "manajemen_logedin";
 		header("location:manajemen/");
 	}else{
-		header("location:login.php?alert=gagal");
+		header("location:login.php?alert=bukan staff");
 	}
 }else{
-	header("location:login.php?alert=gagal");
+	header("location:login.php?alert=Login Gagal");
 }
